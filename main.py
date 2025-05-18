01fcd170df7b838263e718bf6ea661404ead3bee
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-05-18 14:09:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 from kivy.core.window import Window
@@ -9,11 +9,11 @@ from kivy.core.window import Window
 class home(Screen):
     def __init__(self, **kwargs):
         super(home, self).__init__(**kwargs)
-        layout = GridLayout(orientation='lr-tb', rows=4)
-        label = Label(text="Grocer")
-        button1 = Button(text="Create a new item")
-        button2 = Button(text="Current items & inventory")
-        button3 = Button(text="Print a grocery list")
+        layout = FloatLayout(size=(400,400))
+        label = Label(text="Grocer",size_hint=(.75,.2),pos_hint={'x':.125,'y':.7},font_size='100sp')
+        button1 = Button(text="Create a new item",size_hint=(.75,.2),pos_hint={'x':.125,'y':.4})
+        button2 = Button(text="Current items & inventory",size_hint=(.75,.2),pos_hint={'x':.125,'y':.2})
+        button3 = Button(text="Print a grocery list",size_hint=(.75,.2),pos_hint={'x':.125,'y':0})
         button1.bind(on_press=self.go_to_new_item)
         button2.bind(on_press=self.go_to_inventory)
         button3.bind(on_press=self.go_to_list)
@@ -35,11 +35,27 @@ class home(Screen):
 class new_item(Screen):
     def __init__(self, **kwargs):
         super(new_item, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        label = Label(text="Create a new item for your inventory")
-        button = Button(text="Go Back")
+        layout = FloatLayout(size=(400,400))
+        label = Label(text="Create a new item for your inventory",size_hint=(.75,.2),pos_hint={'x':.125,'y':.85})
+        name_ilabel=Label(text="Name:",pos_hint={'x':.19,'y':.8},size_hint=(.05,.2))
+        name_input=TextInput(size_hint=(.5,.05),pos_hint={'x':.25,'y':.8})
+        pkg_unit_ilabel=Label()
+        pkg_unit_input=TextInput(size_hint=(.5,.05),pos_hint={'x':.25,'y':.75})
+        pkg_qty_ilabel=Label()
+        pkg_qty_input=TextInput(size_hint=(.5,.05),pos_hint={'x':.25,'y':.7})
+        inv_unit_ilabel=Label()
+        inv_unit_input=TextInput(size_hint=(.5,.05),pos_hint={'x':.25,'y':.65})
+        inv_qty_ilabel=Label()
+        inv_qty_input=TextInput(size_hint=(.5,.05),pos_hint={'x':.25,'y':.6})
+        button = Button(text="Go Back",size_hint=(.6,.1),pos_hint={'x':.2,'y':0})
         button.bind(on_press=self.go_back)
         layout.add_widget(label)
+        layout.add_widget(name_ilabel)
+        layout.add_widget(name_input)
+        layout.add_widget(pkg_unit_input)
+        layout.add_widget(pkg_qty_input)
+        layout.add_widget(inv_unit_input)
+        layout.add_widget(inv_qty_input)
         layout.add_widget(button)
         self.add_widget(layout)
 
@@ -49,9 +65,9 @@ class new_item(Screen):
 class inventory(Screen):
     def __init__(self, **kwargs):
         super(inventory, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        label = Label(text="Current Items & Inventory")
-        button = Button(text="Go Back")
+        layout = FloatLayout(size=(400,400))
+        label = Label(text="Current Items & Inventory",size_hint=(.75,.2),pos_hint={'x':.125,'y':.8})
+        button = Button(text="Go Back",size_hint=(.6,.1),pos_hint={'x':.2,'y':0})
         button.bind(on_press=self.go_back)
         layout.add_widget(label)
         layout.add_widget(button)
@@ -63,9 +79,9 @@ class inventory(Screen):
 class create_list(Screen):
     def __init__(self, **kwargs):
         super(create_list, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        label = Label(text="Create & Print a new grocery list")
-        button = Button(text="Go Back")
+        layout = FloatLayout(size=(400,400))
+        label = Label(text="Create & Print a new grocery list",size_hint=(.75,.2),pos_hint={'x':.125,'y':.8})
+        button = Button(text="Go Back",size_hint=(.6,.1),pos_hint={'x':.2,'y':0})
         button.bind(on_press=self.go_back)
         layout.add_widget(label)
         layout.add_widget(button)
